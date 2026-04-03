46a9a7d6a6a75864a343f43516426e29e57f7639
yug06 <patilsudhir349@gmail.com>
2026-04-03 14:17:29 +0530

    initial commit

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ const searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", fetchMovie);
 
 async function fetchMovie() {
-
-
-
-
   movieName = inputBox.value.trim();
 
   let response = await fetch(
@@ -17,33 +13,22 @@ async function fetchMovie() {
   let data = await response.json();
 
   displayMovies(data.Search);
-
-
-
-
-
 }
 
 const displayMovies = (movies) => {
+  let movieGrid = document.querySelector(".movie-grid");
+  movieGrid.innerHTML = "";
 
-    let movieGrid = document.querySelector(".movie-grid")
-    movieGrid.innerHTML = ""
-    
-    movies.forEach((movie) => {
-    
-    
+  movies.forEach((movie) => {
     let card = document.createElement("div");
     card.classList.add("movie-card");
-    
+
     card.innerHTML = `<img class="movie-img" src="${movie.Poster}" alt="movie-logo">
     <div class="movie-info">
     <h3>${movie.Title}</h3>
     <p>${movie.Year}</p>
-    </div>`
-    
-    movieGrid.appendChild(card)
-    
-      
-  });
+    </div>`;
 
+    movieGrid.appendChild(card);
+  });
 };
