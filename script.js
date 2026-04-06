8b617333cac068d119a1a8428eb80be9c3cf7468
yug06 <patilsudhir349@gmail.com>
2026-04-06 17:39:45 +0530

    bug fix

--- script.js
+++ script.js
@@ -2,68 +2,116 @@ const inputBox = document.querySelector("#input-box");
 const searchBtn = document.querySelector("#search-btn");
 const dropdown = document.querySelector("#dropdown");
 const modalOverlay = document.querySelector(".modal-overlay");
-
+const movieGrid = document.querySelector(".movie-grid");
 modalOverlay.style.display = "none";
 
-modalOverlay.addEventListener("click", (e) => {
-  if (e.target === modalOverlay) {
-    modalOverlay.style.display = "none";
+
+modalOverlay.addEventListener("click",(e)=>{
+
+if(e.target == modalOverlay){
+  modalOverlay.style.display = "none"
+}
+
+})
+
+
+inputBox.addEventListener("input", () => {
+  let query = inputBox.value.trim();
+  if (query == "") {
+    dropdown.innerHTML = "";
+    return;
+  } else {
+    fetchSuggestion(query);
   }
 });
 
-searchBtn.addEventListener("click", fetchMovie);
-
-async function fetchMovie() {
-  movieName = inputBox.value.trim();
-
-  let response = await fetch(
-    `https://www.omdbapi.com/?s=${movieName}&apikey=256ef16b`,
+const fetchSuggestion = async (query) => {
+  const response = await fetch(
+    `https://www.omdbapi.com/?s=${query}&apikey=256ef16b`,
   );
+  const data = await response.json();
 
-  let data = await response.json();
 
-  displayMovies(data.Search);
+dropdown.innerHTML =  ""
+if(data.Search){
+
+
+
+
+data.Search.forEach(movie=>{
+
+let p = document.createElement("p")
+
+p.innerHTML = movie.Title
+
+dropdown.appendChild(p)
+
+p.addEventListener("click",()=>{
+
+  inputBox.value = p.textContent
+  dropdown.innerHTML = ""
+   fetchMovies()
+
+})
+
+
+
+})
+
 }
 
-const displayMovies = (movies) => {
-  let movieGrid = document.querySelector(".movie-grid");
-  movieGrid.innerHTML = "";
 
+};
+
+const fetchMovies = async () => {
+  const movieName = inputBox.value.trim();
+
+  const response = await fetch(
+    `https://www.omdbapi.com/?s=${movieName}&apikey=256ef16b`,
+  );
+  const data = await response.json();
+
+  displayMovies(data.Search);
+
+};
+
+searchBtn.addEventListener("click", fetchMovies);
+
+const displayMovies = (movies) => {
+  movieGrid.innerHTML = "";
+console.log(movies)
   if (movies) {
     movies.forEach((movie) => {
-      let card = document.createElement("div");
+      const card = document.createElement("div");
       card.classList.add("movie-card");
-      card.dataset.id = movie.imdbID;
 
-      card.addEventListener("click", () => {
-        let movieId = card.dataset.id;
+ card.dataset.id = movie.imdbID;
 
-        modalOverlay.style.display = "flex";
 
-        fetchMovieDetail(movieId);
-      });
+   card.addEventListener("click",()=>{
+
+  let imdbID =  card.dataset.id
+
+   fetchMovieDetails(imdbID)
+
+   })
 
       card.innerHTML = `<img class="movie-img" src="${movie.Poster}" alt="movie-logo">
-    <div class="movie-info">
-    <h3>${movie.Title}</h3>
-    <p>${movie.Year}</p>
-    </div>`;
+  <div class="movie-info">
+  <h3>${movie.Title}</h3>
+  <p>${movie.Year}</p>
+  </div>`;
 
       movieGrid.appendChild(card);
     });
-  } else {
-    console.log("wrong input");
   }
 };
 
-const fetchMovieDetail = async (movieId) => {
-  const img = document.querySelector("#modal-poster");
-  const response = await fetch(
-    `https://www.omdbapi.com/?i=${movieId}&apikey=256ef16b`,
-  );
 
-  const data = await response.json();
-  console.log(data);
+const fetchMovieDetails = async (id) =>{
+  let response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=256ef16b`)
+  let data = await response.json()
+  modalOverlay.style.display = "flex"
 
   document.querySelector("#modal-details").innerHTML = `
    <h2>${data.Title}</h2>
@@ -72,44 +120,7 @@ const fetchMovieDetail = async (movieId) => {
     <p>🎬Director: ${data.Director}</p>
     <p>🕐Release Date: ${data.Runtime}</p>
     <p>📖Plot: ${data.Plot}</p>
+`
+document.querySelector("#modal-poster").src = data.Poster
 
-
-`;
-  img.src = data.Poster;
-};
-
-inputBox.addEventListener("input", () => {
-  let query = inputBox.value.trim();
-
-  if (query == "") {
-    dropdown.innerHTML = "";
-    return;
-  }
-
-  fetchSuggestion(query);
-});
-
-const fetchSuggestion = async (query) => {
-  const div = document.querySelector("#dropdown");
-  div.innerHTML = "";
-  let response = await fetch(
-    `https://www.omdbapi.com/?s=${query}&apikey=256ef16b`,
-  );
-  let data = await response.json();
-
-  if (data.Search) {
-    data.Search.forEach((movie) => {
-      let p = document.createElement("p");
-      p.innerHTML = movie.Title;
-
-      div.appendChild(p);
-      p.addEventListener("click", () => {
-        inputBox.value = p.textContent;
-        div.innerHTML = "";
-        fetchMovie();
-      });
-    });
-  } else {
-    console.log("wrong input");
-  }
-};
+} 
